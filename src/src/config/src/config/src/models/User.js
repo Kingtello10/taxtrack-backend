e352eac57ebb4fc53cd90772bf5c35b56283295d
Kingtello10 <info.hingeconsulthr@gmail.com>
2026-01-12 13:22:47 +0100

    Update User.js

--- src/src/config/src/config/src/models/User.js
+++ src/src/config/src/config/src/models/User.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  fullName: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: 'user' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
 
