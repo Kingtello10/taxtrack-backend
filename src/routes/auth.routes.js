d171f7c7f9675148737abbdcc8d1b885f11ecf8e
Kingtello10 <info.hingeconsulthr@gmail.com>
2026-01-13 15:47:12 +0100

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,4 +4,8 @@ const { register, login } = require('../controllers/auth.controller');
 router.post('/register', register);
 router.post('/login', login);
 
+router.get('/', (req, res) => {
+  res.json({ message: 'Auth API is working' });
+});
+
 module.exports = router;
