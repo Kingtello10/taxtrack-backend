61df6def9f24734ef8273189b7c2dc932a903c38
Kingtello10 <info.hingeconsulthr@gmail.com>
2026-01-13 15:52:05 +0100

    Update and rename TaxRecord.js to Tax.js

---
+++ src/models/Tax.js
@@ -0,0 +1,12 @@
+// src/models/Tax.js
+const mongoose = require('mongoose');
+
+const TaxSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  type: { type: String, enum: ['PAYE', 'VAT', 'Consumption'], required: true },
+  amount: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  details: { type: String }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Tax', TaxSchema);
